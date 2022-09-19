69249786d17cbdb95904359d4430795c03753e12
Maick-Neres <108484775+Maick-Neres@users.noreply.github.com>
2022-09-19 09:44:28 -0300

    fim

--- javascript.js
+++ javascript.js
@@ -1,16 +1,20 @@
 var botao1;
 var botao2;
+var obj;
 var img;
 var number;
 var h1;
-
+var tentativas=0
 function  negaçao(){
   botao1=document.getElementById("nao");
   botao1.style.top=Math.random()*550+"px";
   botao1.style.right=Math.random()*300+"px";
+   obj=document.getElementById("tentativas");
+   tentativas+=1
 }
 
 function aceitar() {
+ 
   botao1=document.getElementById("nao");
   botao2=document.getElementById("sim");
   img=document.getElementById("imagem");
@@ -18,9 +22,17 @@ function aceitar() {
   botao2.style.display="none";
   h1=document.getElementById("titulo").innerHTML="É MEME HAHAHAHA &#x1F601;";
   img.style.display="block"
+  obj.style.display="block"
+
+  if(tentativas<=5){
+    obj.innerHTML+="Tentativas: &#x1F60E;&#x1F525; "+tentativas
+  }else if(tentativas>5 && tentativas<=10){
+    obj.innerHTML+="Tentativas: &#x128532;&#x128532;"+tentativas
+  }else if(tentativas>10){
+    obj.innerHTML+="Tentativas: &#x128557;&#x128557;"+tentativas
 
 }
-
+}
 
 
 
