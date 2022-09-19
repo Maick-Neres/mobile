1da27a4cce881c8fbfad7a713658d3df632ab6bd
Maick-Neres <108484775+Maick-Neres@users.noreply.github.com>
2022-09-19 10:34:54 -0300

    atualizacao do IF

--- javascript.js
+++ javascript.js
@@ -4,27 +4,29 @@ var obj;
 var img;
 var number;
 var h1;
-var tentativas=0
+ var tentativas=0;
+
+
 function  negaçao(){
   botao1=document.getElementById("nao");
   botao1.style.top=Math.random()*550+"px";
   botao1.style.right=Math.random()*300+"px";
    obj=document.getElementById("tentativas");
    tentativas+=1
+   
 }
 
 function aceitar() {
-  var num=0
+ 
   botao1=document.getElementById("nao");
   botao2=document.getElementById("sim");
   img=document.getElementById("imagem");
   botao1.style.display="none";
   botao2.style.display="none";
   h1=document.getElementById("titulo").innerHTML="É MEME HAHAHAHA &#x1F601;";
+  obj=document.getElementById("tentativas");
   img.style.display="block"
- 
-
-  if(tentativas<=5 || num==0){
+  if (tentativas<=5){
     obj.innerHTML+="Tentativas: &#x1F60E;&#x1F525; "+tentativas
     obj.style.display="block"
   }else if(tentativas>5 && tentativas<=10){
