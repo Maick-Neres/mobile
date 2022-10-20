0db3a9ecc4541e30d084224518276cc683ed4f9f
Maick-Neres <108484775+Maick-Neres@users.noreply.github.com>
2022-10-20 16:38:26 -0200

    alteração

--- javascript.js
+++ javascript.js
@@ -1,10 +1,5 @@
-var botao1;
-var botao2;
-var obj;
-var img;
-var number;
-var h1;
- var tentativas=0;
+var
+ botao1, botao2, obj, img, number, h1, tentativas=0;
 
 
 function  negaçao(){
@@ -18,20 +13,20 @@ function  negaçao(){
 
 function aceitar() {
  
-  botao1=document.getElementById("nao");
-  botao2=document.getElementById("sim");
-  img=document.getElementById("imagem");
-  botao1.style.display="none";
-  botao2.style.display="none";
-  h1=document.getElementById("titulo").innerHTML="É MEME HAHAHAHA &#x1F601;";
-  obj=document.getElementById("tentativas");
-  img.style.display="block"
+    botao1=document.getElementById("nao");
+    botao2=document.getElementById("sim");
+    img=document.getElementById("imagem");
+    botao1.style.display="none";
+    botao2.style.display="none";
+    h1=document.getElementById("titulo").innerHTML="É MEME HAHAHAHA &#x1F601;";
+    obj=document.getElementById("tentativas");
+    img.style.display="block"
   if (tentativas>0 && tentativas<=5){
-    obj.innerHTML+="Tentativas: &#x1F60E;&#x1F525; "+tentativas
-    obj.style.display="block"
+      obj.innerHTML+="Tentativas: &#x1F60E;&#x1F525;"+tentativas
+      obj.style.display="block"
   }else if(tentativas>5 && tentativas<=10){
-    obj.innerHTML+="Tentativas: &#x1F614;&#x1F614;"+tentativas
-    obj.style.display="block"
+      obj.innerHTML+="Tentativas: &#x1F614;&#x1F614;"+tentativas
+      obj.style.display="block"
 
   }else if(tentativas>10){
     obj.innerHTML+="Tentativas: &#x1F622;&#x1F622;"+tentativas
